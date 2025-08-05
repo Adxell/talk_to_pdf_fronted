3b8003c0cb8bfe8a6e28a1c009558226c7cef421
Adxell <adxell02@gmail.com>
2025-08-04 21:26:36 -0500

    feat: Added main component to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,14 @@
 import { useState } from "react";
-import Header from "./components/Header"
+import Header from "./components/app/Header"
+import Main from "./components/app/Main";
 
 function App() {
   const [isDarkMode, setIsDarkMode] = useState(true);
+  
   return (
     <div className={`flex flex-col h-screen font-sans bg-gray-100 dark:bg-gray-900 text-gray-900 dark:text-gray-100 transition-colors duration-300`}>
       <Header isDarkMode={isDarkMode} setIsDarkMode={setIsDarkMode} />
-      {/* <main className="flex-1 flex flex-col items-center justify-center p-4 overflow-hidden">
-        <div className="w-full max-w-4xl h-full flex flex-col bg-white dark:bg-gray-800 rounded-2xl shadow-2xl border border-gray-200 dark:border-gray-700">
-          {!isPdfUploaded ? (
-            <PDFUpload onPdfUpload={handlePdfUpload} isLoading={isLoading} />
-          ) : (
-            <>
-              <ChatHeader fileName={"sample-document.pdf"} onReset={resetChat} />
-              <ChatWindow messages={messages} isLoading={isLoading} />
-              <ChatInput
-                userInput={userInput}
-                setUserInput={setUserInput}
-                onSubmit={handleSubmit}
-                isLoading={isLoading}
-              />
-            </>
-          )}
-           {error && <ErrorMessage message={error} onClose={() => setError(null)} />}
-        </div>
-      </main> */}
+      <Main/>
     </div>
   )
 }
